f459c3e86e9ee21c5bc3eb6a6d7a13d5b150f6fd
vvsashish <vvsashish96@gmail.com>
2025-01-12 19:12:41 +0530

    added cors middleware

--- server/Server.js
+++ server/Server.js
@@ -31,6 +31,20 @@ app.use(
   })
 );
 
+app.use((req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://comic-confessions-frontend.vercel.app"
+  );
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, authtoken"
+  );
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
+
 const httpServer = createServer(app);
 
 app.use(express.json());
